f90278f350e8d3278cbb3ff5ece3b5c51b2ac1d3
Borisstoy <boris.stoy@gmail.com>
2017-09-18 10:45:39 -0400

    :computer: Include geocoder helper

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,21 @@
 class PlacesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
 
+  include GeocoderHelper
+
   def index
-    @places = Place.all.first
+    center_map(Place)
+    filters
+  end
+
+  def filters
+    @filtered_places = find_places
+    display_places_markers(@filtered_places)
+  end
+
+  private
+
+  def find_places
+    Place.all
   end
 end
---
+++ app/helpers/geocoder_helper.rb
@@ -0,0 +1,52 @@
+module GeocoderHelper
+  def center_map(collection)
+    location = params ? params : "Montreal, QC"
+    within_area(collection, geocoder(location))
+  end
+
+  def within_area(collection, box)
+    @geocoded_places = collection.within_bounding_box(box)
+  end
+
+  def geocoder(location)
+    center = Geocoder.search(location)
+    bounds = center.first.geometry['bounds'] || Geocoder.search("Québec, QC, Canada").first.geometry['bounds']
+    box = [
+      bounds['southwest']['lat'],
+      bounds['southwest']['lng'],
+      bounds['northeast']['lat'],
+      bounds['northeast']['lng'],
+    ]
+  end
+
+  def build_places_markers(places)
+    places_coordinates = []
+    places_markers     = []
+
+    places.each do |place|
+      coord = {
+        lat: place.lat,
+        lng: place.lng
+      }
+      places_coordinates << coord
+
+      marker = {}
+
+      marker[:place_lat] = place.lat
+      marker[:place_lng] = place.lng
+      marker[:infowindow] = render_to_string(
+                                partial: "infowindow.html.erb",
+                                formats: [:html],
+                                layout: false,
+                                locals: {place: place},
+                                cache: true)
+
+      places_markers << marker
+    end
+    return places_markers
+  end
+
+  def display_places_markers(places)
+    @places_markers = build_places_markers(@filtered_places)
+  end
+end
