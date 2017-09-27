b5056f7ce380affe75864804ff758323e6d27fe3
Borisstoy <boris.stoy@gmail.com>
2017-09-27 13:57:12 -0400

    :computer: Allow respond to js for filtered results

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,21 +1,35 @@
 class PlacesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index ]
+  skip_before_action :authenticate_user!, only: [ :index, :filters ]
+  before_action :set_places, only: [:show]
 
   include GeocoderHelper
 
   def index
     center_map(Place)
+    places_markers
     filters
   end
 
-  def filters
-    @filtered_places = find_places
+  def show
+  end
+
+  def places_markers
+    @filtered_places = Place.all
     display_places_markers(@filtered_places)
+  end
+
+  def filters
+    places_kind = params[:kind] if params[:kind].present?
+    @places_by_kind = Place.where(kind: places_kind)
+    respond_to do |format|
+      format.js
+      format.html
+    end
   end
 
   private
 
-  def find_places
-    @filtered_places = Place.all
+  def set_places
+    @place = Place.find params[:id]
   end
 end
