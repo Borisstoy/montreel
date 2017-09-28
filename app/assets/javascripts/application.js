17eda0ced5f51cd01d0ea3b1b374d519e1b97f9f
Borisstoy <boris.stoy@gmail.com>
2017-09-28 18:10:06 -0400

    :computer: Add easeScroll dependency

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 //= require jquery
 //= require jquery_ujs
+//= require easeScroll
 //= require snazzy-info-window/dist/snazzy-info-window.min.js
 //= require bootstrap-sprockets
 //= require_tree .
