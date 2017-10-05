ca781dd6cd74379ae8d9714a30e1db3d2e60fdf7
Borisstoy <boris.stoy@gmail.com>
2017-10-04 22:19:54 -0400

    :computer::lipstick: Display all monuments infos

--- app/decorators/place_decorator.rb
+++ app/decorators/place_decorator.rb
@@ -19,9 +19,83 @@ class PlaceDecorator < Draper::Decorator
     end
   end
 
-  def artist_name
-    if monument_summary.artist_name.present?
-      monument_summary.artist_name
+  def location
+    if borough.present? && monument_summary.park.present?
+      h.content_tag(:span) do
+        "#{borough + ', ' + monument_summary.park}"
+      end
+    elsif borough.present?
+      h.content_tag(:span) do
+        "#{borough}"
+      end
+    elsif monument_summary.park.present?
+      h.content_tag(:span) do
+        "#{monument_summary.park}"
+      end
+    elsif monument_summary.building.present?
+      h.content_tag(:span) do
+        "#{monument_summary.building}"
+      end
+    end
+  end
+
+  def artist_full_name
+    if monument_summary.artist_name.present? && monument_summary.artist_last_name.present?
+      h.content_tag(:span, class: 'artist-name') do
+        monument_summary.artist_name + " " + monument_summary.artist_last_name
+      end
+    elsif monument_summary.artist_name.present?
+      h.content_tag(:span, class: 'artist-name') do
+        monument_summary.artist_name
+      end
+    else
+      h.content_tag(:span, class: 'artist-name') do
+        monument_summary.artist_last_name
+      end
+    end
+  end
+
+  def collection_name
+    if monument_summary.collection_name_fr.present?
+      monument_summary.collection_name_fr
+    end
+  end
+
+  def category
+    if monument_summary.category_fr.present?
+      monument_summary.category_fr
+    end
+  end
+
+  def sub_cat_fra
+    if monument_summary.sub_category_fr.present?
+      monument_summary.sub_category_fr
+    end
+  end
+
+  def material_fr
+    if monument_summary.materials_fr.present?
+      monument_summary.materials_fr
+    end
+  end
+
+  def tech_fr
+    if monument_summary.tech_fr.present?
+      monument_summary.tech_fr
+                      .gsub(';', ' ~')
+                      .downcase.truncate(85, omission: '...')
+    end
+  end
+
+  def park
+    if monument_summary.park.present?
+      monument_summary.park
+    end
+  end
+
+  def building
+    if monument_summary.building.present?
+      monument_summary.building
     end
   end
 end
