8aa72b8398572184f57c02b77315ec5846fd8a1f
Borisstoy <boris.stoy@gmail.com>
2017-10-08 11:33:56 -0400

    :computer::art: Handle ajax request for mon categories, refacto

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,11 +1,12 @@
 class PlacesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index, :filters, :fetch_place_data ]
+  skip_before_action :authenticate_user!, only: [ :index, :filters ]
   before_action :set_places, only: [:show]
 
   include GeocoderHelper
 
   def index
     center_map(Place)
+    monument_sub_categories_names
     places_markers
     filters
     respond_to do |format|
@@ -15,33 +16,50 @@ class PlacesController < ApplicationController
   end
 
   def places_markers
-    @filtered_places = Place.all
-    @filtered_places = @filtered_places.decorate
-    display_places_markers(@filtered_places)
+    @all_places = Place.all
+    @all_places = @all_places.decorate
+    display_places_markers(@all_places)
   end
 
   def filters
-    if params[:kind].present?
-      sanitized_place_kind = ActiveRecord::Base.connection.quote params[:kind]
+    place_kind            = params[:kind]
+    monument_sub_categoy  = params[:monument_cat]
 
-      query =
-        "SELECT *
-         FROM places
-         WHERE places.kind = #{sanitized_place_kind}
-         ORDER BY name ASC"
-      @places_by_kind = Place.find_by_sql(query)
-      @places_by_kind = @places_by_kind.map {|place| place.decorate }
+    @filtered_places = []
 
-      respond_to do |format|
-        format.js
-        format.html
-      end
+    if place_kind.present?
+      @filtered_places = Place.find_by_kind(place_kind)
+    elsif monument_sub_categoy.present?
+      monuments_sub_categories_filters(monument_sub_categoy)
+    end
+
+    @filtered_places = @filtered_places.map {|place| place.decorate }
+
+    respond_to do |format|
+      format.js
+      format.html
     end
 
     gon.markers = @places_markers
   end
 
   private
+
+  def monuments_sub_categories_filters(category_params)
+    if category_params == (I18n.t 'all_categories')
+      @filtered_places = Place.find_by_kind('Monument')
+    elsif category_params != @all_categories
+      @filtered_places = Place.monument_by_category(category_params)
+    end
+  end
+
+  def monument_sub_categories_names
+    @all_categories = (I18n.t 'all_categories')
+    @monument_sub_categories_names =
+      MonumentSummary.pluck(:sub_category_fr)
+                     .uniq
+                     .unshift @all_categories
+  end
 
   def set_places
     @place = Place.find params[:id]
