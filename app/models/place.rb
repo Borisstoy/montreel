d67929c69ec2bc645a5227c594d74e5ba512858e
Borisstoy <boris.stoy@gmail.com>
2017-10-13 19:14:43 -0400

    :computer: Add associations to places

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,8 +3,10 @@ class Place < ApplicationRecord
   after_validation :reverse_geocode
 
   has_one :monument_summary
+  has_one :wall_summary
 
   accepts_nested_attributes_for :monument_summary
+  accepts_nested_attributes_for :wall_summary
 
   scope :find_by_kind, -> (kind) { where(kind: kind).order('name ASC') }
   scope :monument_by_category, -> (category) { joins(:monument_summary).where(kind: 'Monument')
