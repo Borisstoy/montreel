f005c36cf1cb9f4eb3e2a61cda4c5107a5fc1bc6
Borisstoy <boris.stoy@gmail.com>
2017-10-14 11:59:09 -0400

    :computer: Create contacts model and controller

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,42 @@
+class ContactsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :new, :create ]
+  before_action :set_contact, only: [:edit]
+
+  def new
+    @new_contact = Contact.new
+  end
+
+  def create
+    @new_contact = Contact.new contacts_params
+
+    respond_to do |format|
+      if @new_contact.save
+        format.html { redirect_to root_path }
+        format.js
+        flash[:notice] = t('controllers.contacts.success')
+        AdminNotificationMailer.new_contact_notification(@new_contact).deliver
+      else
+        format.html do
+          flash[:alert] = t('controllers.contacts.fail')
+          render :new
+        end
+        format.js { render :new }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def contacts_params
+    params.require(:contact).permit(:name,
+                                    :email,
+                                    :content)
+  end
+
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
+end
