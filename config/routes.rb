d988e2263811be2b565182816a1c6503335d8531
Borisstoy <boris.stoy@gmail.com>
2017-10-13 22:07:37 -0400

    :computer: Add conditionnal routes based on environment

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
     ActiveAdmin.routes(self)
     devise_for :users
 
-    root to: 'pages#home'
+    if Rails.env.development?
+      root to: 'places#index'
+    elsif Rails.env.production?
+      root to: 'pages#home'
+    end
     resources :places
     post 'places/filters', 'places#filters'
     post 'places/monuments_filter', 'places#monuments_filter'
