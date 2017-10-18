aebdbc898913566682151481312d1cf3096b6806
Borisstoy <boris.stoy@gmail.com>
2017-10-17 21:39:04 -0400

    :bug: Change sendgrid domain name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.sendgrid.net",
     :port                 => 587,
-    :domain               => "rocket-mvp.herokuapp.com",
+    :domain               => "montreel.herokuapp.com",
     :user_name            => ENV['SENDGRID_USERNAME'],
     :password             => ENV['SENDGRID_PASSWORD'],
     :authentication       => "plain",
